e85c108fe4501a742bc373a6cc390fe3542fd578
Soft21c <105536934+Soft21c@users.noreply.github.com>
2025-01-11 21:20:10 +0900

    Waktaverse VideosㅣBeta 1.2

--- src/app/api/auth/auth.js
+++ src/app/api/auth/auth.js
@@ -1,8 +1,7 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import { SupabaseAdapter } from "@auth/supabase-adapter";
-import { createSupabaseClient } from "@/utils/supabase";
- 
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
 })
--- src/utils/authenticationredirect.js
+++
@@ -1,6 +0,0 @@
-export function authentication(url) {
-    const authentication = process.env.authentication_URL?.split(",") || [];
-
-    return authentication.includes(url);
-}
-  
