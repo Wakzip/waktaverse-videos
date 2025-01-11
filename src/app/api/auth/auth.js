43f1ea90d8423b2c9025cbbcf7ae97c950f94be8
Soft21c <105536934+Soft21c@users.noreply.github.com>
2025-01-11 20:09:41 +0900

    Waktaverse Videosㅣ Beta 1.2

--- src/app/api/auth/auth.js
+++ src/app/api/auth/auth.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
-
+import { SupabaseAdapter } from "@auth/supabase-adapter";
+import { createSupabaseClient } from "@/utils/supabase";
+ 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
 })
