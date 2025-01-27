02131622219d2a950a22b4ec1cb0edb9d5ba8191
Soft21c <105536934+Soft21c@users.noreply.github.com>
2025-01-28 00:05:34 +0900

    하이라이트 추가

--- src/app/page.js
+++ src/app/page.js
@@ -25,9 +25,9 @@ export default function HomePage() {
       {session ? (
         <>
         <section className="home">
-            <div className='banner-container'>
+            <div className='banner-container_logined'>
               <div className='banner-section-logined'>
-                <h1>반가워요, {session.user.name}님!</h1>
+                <h1>안녕하세요, <span className='highlight_login'>{session.user.name}</span>님!</h1>
               </div>
               <div className='hello'>
                 <p><br></br>안녕하세요, 팬치와 이파리 여러분!</p>
@@ -60,39 +60,6 @@ export default function HomePage() {
         </>
       ) : (
         <>
-        <section className="home">
-            <div className='banner-container_logined'>
-              <div className='banner-section-logined'>
-                <h1>안녕하세요, <span className='highlight_login'>session.user.name</span>님!</h1>
-              </div>
-              <div className='hello'>
-                <p><br></br>안녕하세요, 팬치와 이파리 여러분!</p>
-                <p>Waktaverse Videos에 오신 것을 환영해요!</p>
-                <p><br></br>아래 버튼으로 로그인하고,<br></br>Waktaverse Videos의 모든 기능을 이용해보세요!</p>
-              </div>
-              <button onClick={clickLogin} className="login-home">
-                <strong>
-                  <CiLogin className="login-icon" /> 로그인
-                </strong>
-              </button>
-            </div>
-            <img src="/wakta_videos_home_banner.png" alt="배너 이미지" className='home_banner_picture'/>
-          </section>
-          <section className="video">
-            <h1 className='video_section_title'>영상</h1>
-            <p className='video_section_subtitle'></p>
-          </section>
-          <section className="playlist">
-            
-          </section>
-          <footer>
-            <div className='footer-section'>
-              <img src="/waktaverse_videos_new_logo_gray_text.png" alt="로고" className='footer-logo'/>
-              <p className='madeby'><strong>Copyright ⓒ Team WakzipㆍWaktaverse Videos.</strong></p>
-              <p className='info_service'>Waktaverse Videos는 왁타버스에서 제공하는 서비스가 아닙니다.</p>
-              <p className='info_contact_mail'><a href='mailto:contact@wakzip.kr?subject=[문의]?body=문의를 작성하시고 전송하시면 Team Wakzip 서비스 팀으로 자동 전송됩니다.'>[문의] contact@wakzip.kr</a></p>
-            </div>
-          </footer>
           <section className="home">
             <div className='banner-container'>
               <div className='banner-section'>
